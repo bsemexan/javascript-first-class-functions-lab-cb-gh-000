57ea482237954594c87dfe3a55eeb9cab3cbb53e
Berry  Semexan <bsemexan@gmail.com>
2018-04-29 17:33:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function createMultiplier(multiplierValue){
   return (value) => value * multiplierValue;
 }
  var doubler = createMultiplier(2);
- var tripler = createMultiplier(3)
+ var tripler = createMultiplier(3);
 
  function multiplier(a, b){
    return a * b
